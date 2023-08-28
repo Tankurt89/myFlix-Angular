ab3271455821dc6ba9ff4087d94775292e23a92d
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-08-28 12:11:40 -0400

    Additonal Api Calls
    Additional Api calls added, previous commit was incorrect as it did not have the endpoints needed.

--- myFlix-angular-client/src/app/fetch-api-data.service.ts
+++ myFlix-angular-client/src/app/fetch-api-data.service.ts
@@ -21,6 +21,13 @@ export class UserRegistrationService {
     )
   }
 
+  public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails)
+    return this.http.post(apiUrl + 'login?' + new URLSearchParams(userDetails), {}).pipe(
+      catchError(this.handleError)
+    )
+  }
+  // The following code will make the api calls to the end points
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token')
     return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
@@ -32,6 +39,109 @@ export class UserRegistrationService {
       )
   }
 
+  getOneMovie(title: string): Observable<any> {
+    const token = localStorage.getItem('token')
+    return this.http.get(apiUrl + 'movies/' + title, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  getOneDirector(directorName: string): Observable<any>{
+    const token = localStorage.getItem('token')
+    return this.http.get(apiUrl + 'movies/director/' + directorName, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  getOneGenre(genreName: string): Observable<any>{
+    const token = localStorage.getItem('token')
+    return this.http.get(apiUrl + 'movies/genres;' + genreName, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  getOneUser(username: string): Observable<any>{
+    const user = localStorage.getItem('Username')
+    const token = localStorage.getItem('token')
+    return this.http.get(apiUrl + 'users/' + username, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  getFavoriteMovie(username: string): Observable<any>{
+    const token = localStorage.getItem('token')
+    return this.http.get(apiUrl + 'users/' + username, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        map((data) => data.FavoriteMovies),
+        catchError(this.handleError)
+      )
+  }
+
+  addFavoriteMovie(movieId: string, username: string): Observable<any>{
+    const token = localStorage.getItem('token')
+    return this.http.post(apiUrl + `users/${username}/movies/${movieId}`, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  deleteFavoriteMovie(movieId: string, username: string): Observable<any>{
+    const token = localStorage.getItem('token')
+    return this.http.delete(apiUrl + `users/${username}/movies/${movieId}`, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  editUser(updateUser: any): Observable<any>{
+    const user = JSON.parse(localStorage.getItem('user') || '{}')
+    const token = localStorage.getItem('token')
+    return this.http.put(apiUrl + 'users/' + user.Username, updateUser, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+
+  deleteUser(): Observable<any> {
+    const user = JSON.parse(localStorage.getItem('user') || '{}')
+    const token = localStorage.getItem('token')
+    return this.http.delete(apiUrl + 'users/' + user._id, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })}).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError)
+      )
+  }
+  // end code for the api endpoints
   private extractResponseData(res: Response): any {
     const body = res;
     return body || { }
