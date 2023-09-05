588eafb5c129ed4a2f79988e0fea4c8c70df4e89
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-09-05 14:04:40 -0400

    Welcome page/Movie card creation
    Created the welcome page and the movie card pages for the angular project.

--- myFlix-angular-client/src/app/user-login-form/user-login-form.component.ts
+++ myFlix-angular-client/src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import {  UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -15,8 +16,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
-
+    public snackBar: MatSnackBar,
+    private router: Router ){ }
   ngOnInit(): void {
   }
 
@@ -29,6 +30,7 @@ export class UserLoginFormComponent implements OnInit {
       this.snackBar.open('you\'ve been logged in', 'OK', {
         duration: 2000
       });
+      this.router.navigate(['movies'])
     }, (data) => {
       this.snackBar.open(data, 'OK', {
         duration: 2000
