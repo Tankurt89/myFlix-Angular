0d9596623acba49c84d1d194d4190b2fecdc154e
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-09-06 12:06:44 -0400

    movie card update with movie details
    movie card updated to include the details for director and genre.

--- myFlix-angular-client/src/app/movie-card/movie-card.component.ts
+++ myFlix-angular-client/src/app/movie-card/movie-card.component.ts
@@ -1,24 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService) { }
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      this.router.navigate(['welcome']);
+      return;
+    }
     this.getMovies();
   }
 
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
-      console.log(this.movies);
-      return this. movies;
+      return this.movies;
+    });
+  }
+
+  openGenreDialog(Genre: any): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: {
+        Name: Genre.Name,
+        Description: Genre.Description,
+      }
+    })
+  }
+
+  openSynopsisDialog(synopsis: string): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: {
+        title: "Description",
+        content: synopsis,
+      }
+    })
+  }
+
+  openDirectorDialog(director: any): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: {
+        title: director.Name,
+        content: director.Bio,
+      }
+    })
+  }
+
+  isFavorite(id: string): boolean {
+    return this.fetchApiData.isFavoriteMovie(id)
+  }
+
+  removeFavorite(id: string): void {
+    this.fetchApiData.deleteFavoriteMovie(id).subscribe(() => {
+      this.snackBar.open('removed from favorites', 'OK', {
+        duration: 2000
+      })
+    });
+  }
+
+  addFavorite(id: string): void {
+    this.fetchApiData.addFavoriteMovie(id).subscribe(() => {
+      this.snackBar.open('added to favorites', 'OK', {
+        duration: 2000
+      })
     });
   }
 }
